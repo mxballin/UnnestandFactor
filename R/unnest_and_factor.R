c8d11dfa2a786d972760cb97f55284f059073d0d
Mya Ballin <mxballin@gmail.com>
2021-11-03 23:07:05 -0700

    Fixing errors

--- R/unnest_and_factor.R
+++ R/unnest_and_factor.R
@@ -8,11 +8,14 @@
 #' @param var The target variable from that tibble - called 'var' as short for variable
 #' @param sep The separator string that the function should use to identify nested values; include quotations for proper usage - called 'sep' to mirror dplyr language
 #'
+#' @import dplyr tidyr
+#' @import rlang
+#'
 #' @return The function returns the unnested tibble.
 #' @export
-#' @examples unnest_and_factor(steam_games, game_details,",")
+#' @examples \dontrun{ unnest_and_factor(steam_games, game_details,",")
 #' unnest_and_factor(apt_buildings, parking_type, ",")
-#' unnest_and_factor(apt_buildings, bike_parking, " and ")
+#' unnest_and_factor(apt_buildings, bike_parking, " and ")}
 #'
 #' @references
 #' Syntax suggested on StackOverflow by LyzandeR
@@ -25,8 +28,8 @@ unnest_and_factor <- function(df,var,sep) { #where df is the dataset, var is the
   if (!check[[1]])
     warning("The variable you have selected to unnest is not a character. It will be converted to a character as part of the evaluation of this function.") #warning that the function converts the existing non-character variable, if applicable, to a character in order to work
   unnested_factored <- df %>%
-    mutate({{var}}:=strsplit(as.character({{var}}), sep))%>%  #separating the observations on the given separator
-    unnest({{var}}) %>% #unnesting the separated observations
-    mutate({{var}}:=as.factor({{var}})) #factoring the observations for grouping capabilities
+    dplyr::mutate({{var}}:=strsplit(as.character({{var}}), sep))%>%  #separating the observations on the given separator
+    tidyr::unnest({{var}}) %>% #unnesting the separated observations
+    dplyr::mutate({{var}}:=as.factor({{var}})) #factoring the observations for grouping capabilities
   return(unnested_factored) #returning the unnested and factored tibble
 }
